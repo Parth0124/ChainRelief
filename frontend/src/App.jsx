86166ffead835c69de409294006a3b79235ab2c6
Parth0124 <abhangparth@gmail.com>
2025-03-19 19:19:05 +0530

    working on sidebar and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CampaignDetails, Home, Profile, CreateCampaign } from "./pages";
+import {Sidebar, Navbar} from './components'
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold  hover:text-blue-600 dark:hover:text-blue-400">
-        Hello World
-      </h1>
-    </div>
+    <BrowserRouter>
+      <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+        <div className="sm:flex hidden mr-10 relative">
+          {/* Sidebar component should go here */}
+          <Sidebar />
+        </div>
+        <div className="flex max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+          {/* Navbar component should go here */}
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-campaign" element={<CreateCampaign />} />
+            <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/CreateCampaign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateCampaign() {
+  return (
+    <div>
+      <h1>Create Cmapaign</h1>
+    </div>
+  )
+}
+
+export default CreateCampaign
